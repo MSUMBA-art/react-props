d3863eb32b043ba267328d96e1294210f3064ee9
ALEXANDER <msumba83@gmail.com>
2022-11-19 15:26:09 +0200

    first commit

---
+++ src/components/Info.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import logo from "../assets/aj-2.jpg";
+
+function Info() {
+  return (
+      <div>
+         <img src={logo} alt="logo"  className='logo'/>
+    </div>
+  )
+}
+
+export default Info
